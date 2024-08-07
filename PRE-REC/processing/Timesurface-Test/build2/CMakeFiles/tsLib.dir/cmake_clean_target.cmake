36fedd8122c57cd9deb6f0d1deec339b293a938e
John Wulff <jmwulff.22@gmail.com>
2024-08-06 21:26:20 -0600

    testing optimization with new v3.0.1 recording

--- PRE-REC/processing/Timesurface-Test/build2/CMakeFiles/tsLib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libtsLib.a"
-)
--- PRE-REC/processing/Timesurface-Test/filepath_config.hpp
+++ PRE-REC/processing/Timesurface-Test/filepath_config.hpp
@@ -8,9 +8,9 @@ using namespace std;
 // duration to wait until starting Metavision::Camera object
 int start_delay = 1;
 // source recording.raw path
-string rec_path = "/home/theia-onboard-media/input/recordings/frame_clip__v3.0.1.raw";
+string rec_path = "/home/theia-onboard-media/input/recordings/ky_rec2_301_5s.raw";
 // source recording.raw nickname
-string rec_name = "Non-star picture frame testing clip (Metavision v3.0.1)";
+string rec_name = "Kentucky mountless star recording (Metavision v3.0.1)";
 // saved recording.raw path (will just duplicate original recording)
 string output_path = "/home/theia-onboard-media/output/recordings/asynchronous_timesurface_test__recent.raw";
 
